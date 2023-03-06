3833f60dcbbe2713d353fcee0446cdb43a58cf11
lkzwc <lkzwc@outlook.com>
2023-03-06 22:55:03 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,12 @@ export default defineUserConfig({
   description: "京东优惠券群,每日分享京东优惠,加入微信群畅享各种购物优惠",
   head: [
     ["meta", { name: "baidu-site-verification", content: "codeva-JBFEaDTQdi" }],
+    ["meta", { name: "baidu-site-verification", content: "codeva-JBFEaDTQdi" }],
     [
       "meta",
       {
         name: "360-site-verification",
-        content: "94c9f33945f66ef0cb7c21955804be61",
+        content: "5616ac48448eb3c86f4d5dd842fb13c6",
       },
     ],
     ["meta", { name: "bytedance-verification-code", content: "e0qMeNiQ" }],
